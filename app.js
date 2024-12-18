194964aea32ce6c60ad5eba49be01b336e140824
Californiae <1288217@ukr.net>
2024-12-18 21:48:22 +0100

    fix(no rule): fix section TODO

--- app.js
+++ app.js
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+
+    listItem.className='list-item';
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='task all-lbl';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task all-inp";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit all-btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete all-btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = 'del-img';
     deleteButton.appendChild(deleteButtonImg);
 
 
